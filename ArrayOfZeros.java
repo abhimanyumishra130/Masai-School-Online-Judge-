a67829c1f8432231ec15116580acbcebc0cad254
Abhimanyu Mishra <83170741+abhimanyumishra130@users.noreply.github.com>
2021-06-15 19:32:00 +0530

    Create ArrayOfZeros.java

---
+++ ArrayOfZeros.java
@@ -0,0 +1,41 @@
+/*
+Array of Zeroes
+Description
+
+You have an array of non-negative integers. You can perform one operation on it in which you can choose i-th index with 0 <= i < n and subtract 1 from both i-th and (i+1) th index of array.
+
+Your task is to write a program that can comment if it is possible to get an array of all zeros after several operations.
+
+
+Input
+Input Format
+
+The first line of test case is a number N. (0 < N <= 10000) The next line is N non-negative integers, 0 <= a_i <= 109
+
+Constraints
+
+0 < N <= 10000
+
+
+Output
+Output Format
+
+If it can be modified into all zeros with several operations output “YES” in a single line, otherwise output “NO” instead.
+
+
+Sample Input 1 
+
+2
+1 2
+Sample Output 1
+
+NO
+Sample Input 2 
+
+2
+2 2
+Sample Output 2
+
+YES
+
+*/
